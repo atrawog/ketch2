9de8618d645da03bc72a4c40e36d0b6109eb0d15
Andreas Trawoeger <atr@nessus.at>
2019-10-23 14:02:47 +0200

    Fix sudo authkey

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -3,4 +3,4 @@ sudo dnf makecache
 sudo dnf install -y openssh-server ansible curl python3-pip
 sudo systemctl enable --now sshd
 sudo sh -c 'cat /dev/zero | ssh-keygen -q -N ""'
-sudo curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys
+sudo sh -c 'curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys'
