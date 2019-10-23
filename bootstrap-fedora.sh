a1dd8fb55f3f61d627648e3ef64b641c5da64e33
Andreas Trawoeger <atr@nessus.at>
2019-10-23 14:12:24 +0200

    Sudo fixes

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo dnf makecache
-sudo dnf install -y openssh-server ansible curl python3-pip
-sudo systemctl enable --now sshd
+sudo sh -c 'dnf makecache'
+sudo sh -c 'dnf install -y openssh-server ansible curl python3-pip'
+sudo sh -c 'systemctl enable --now sshd'
 sudo sh -c 'cat /dev/zero | ssh-keygen -q -N ""'
 sudo sh -c 'curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys'
-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
-pip3 install invoke certifi hetzner
-dnf upgrade -y
-dnf clean all -y
+sudo sh -c 'alternatives --install /usr/bin/python python /usr/bin/python3.7 2'
+sudo sh -c 'pip3 sudo install invoke certifi hetzner'
+sudo sh -c 'dnf upgrade -y'
+sudo sh -c 'dnf clean all -y'
