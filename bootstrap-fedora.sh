1e4802239f633a328d1740ce100e036c5543fb06
Andreas Trawoeger <atr@nessus.at>
2019-10-23 14:23:41 +0200

    Set user authkeys

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -4,6 +4,7 @@ sudo sh -c 'dnf install -y openssh-server ansible curl python3-pip'
 sudo sh -c 'systemctl enable --now sshd'
 sudo sh -c 'cat /dev/zero | ssh-keygen -q -N ""'
 sudo sh -c 'curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys'
+curl -L https://github.com/atrawog.keys >> ~/.ssh/authorized_keys
 sudo sh -c 'alternatives --install /usr/bin/python python /usr/bin/python3.7 2'
 sudo sh -c 'pip3 install invoke certifi hetzner'
 sudo sh -c 'dnf upgrade -y'
