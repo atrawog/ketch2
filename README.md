41b8e2d0b8b49feae07195195aa75a9a0e2d88d2
Andreas Trawoeger <atr@nessus.at>
2019-10-23 14:07:49 +0200

    Use python3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ketch2
+
+Bootstrap with curl -s https://raw.githubusercontent.com/atrawog/ketch2/master/bootstrap-fedora.sh | sh
--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -4,3 +4,7 @@ sudo dnf install -y openssh-server ansible curl python3-pip
 sudo systemctl enable --now sshd
 sudo sh -c 'cat /dev/zero | ssh-keygen -q -N ""'
 sudo sh -c 'curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys'
+alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+pip3 install invoke certifi hetzner
+dnf upgrade -y
+dnf clean all -y
