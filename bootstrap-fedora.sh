ba3e8e480e537b2e61f733b400dc4f05cdb0ec6f
Andreas Trawoeger <atr@nessus.at>
2019-10-23 13:59:16 +0200

    Fix Sudo Keygen

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -2,5 +2,5 @@
 sudo dnf makecache
 sudo dnf install -y openssh-server ansible curl python3-pip
 sudo systemctl enable --now sshd
-sudo cat /dev/zero | ssh-keygen -q -N ""
+sudo sh -c 'cat /dev/zero | ssh-keygen -q -N ""'
 sudo curl -L https://github.com/atrawog.keys >> /root/.ssh/authorized_keys
